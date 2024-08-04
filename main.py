77e095dae732734cb27cc8a50233624312bb29bb
Kauã Ferreira Marques <116847328+KauaFMarques@users.noreply.github.com>
2024-08-04 19:17:38 +0000

    132 finalizado. crianco rotas para retorno de vagas em formato json

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
@@ -34,6 +34,9 @@ VAGAS = [{
 def hello_world():
   return render_template("home.html", vagas=VAGAS)
 
+@app.route("/vagas")
+def listar_vagas():
+  return jsonify(VAGAS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080)
