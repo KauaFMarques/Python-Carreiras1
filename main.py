a85c9c4cfab9100b1bedf5364ee230ac67b97381
Kauã Ferreira Marques <116847328+KauaFMarques@users.noreply.github.com>
2024-08-04 18:40:06 +0000

    aula 128. criando Listas em python

--- main.py
+++ main.py
@@ -7,6 +7,36 @@ app = Flask(__name__)
 def hello_world():
   return render_template("home.html")
 
+VAGAS= [
+  {
+    'id':1,
+    'titulo':'analista de dados',
+    'localidade':'Rio de Janeiro',
+    'salario':'R$ 5.000,00'
+  },
+  {
+    'id':2,
+    'titulo':'desenvolvedor frontend',
+    'localidade':'Parana',
+    'salario':'R$ 3.500,00'},
+  {
+    'id':3,
+    'titulo':'analista de dados',
+    'localidade':'sao paulo',
+    'salario':'R$ 5.000,00'
+  },
+  {
+    'id':4,
+    'titulo':'analista de dados',
+    'localidade':'Rio de Janeiro',
+    'salario':'R$ 3.500,00'},
+  {
+    'id':5,
+    'titulo':'UI/UIX desing',
+    'localidade':'Malta',
+    'salario':'R$ 2.500,00'}
+]
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080)
